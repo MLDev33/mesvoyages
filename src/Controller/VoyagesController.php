d622d30a8c640d4bd6f4b56ba5dd109175407d3f
MLDev33 <ypmarielaure@yahoo.fr>
2025-02-05 09:19:58 +0100

    Phase 9 : ORM (filtrage des enregistrements).

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -35,12 +36,33 @@ class VoyagesController extends AbstractController {
     public function __construct(VisiteRepository $repository) {
         $this->repository = $repository;
     }
-
+    
+    /**
+     * 
+     * @param type $champ
+     * @param type $order
+     * @return Response
+     */
     #[Route("/voyages/tri/{champ}/{order}", name: "voyages.sort")]
     public function sort($champ, $order): Response {
         $visites = $this->repository->findAllOrderBy($champ, $order);
         return $this->render("pages/voyages.html.twig", [
                     'visites' => $visites
+        ]);
+    }
+
+    /**
+     * 
+     * @param type $champ
+     * @param Request $request
+     * @return Response
+     */
+    #[Route("/voyages/recherche/{champ}", name: "voyages.findallequal")]
+    public function findAllEqual($champ, Request $request): Response {
+        $valeur = $request->get("recherche");
+        $visites = $this->repository->findByEqualValue($champ, $valeur);
+        return $this->render("pages/voyages.html.twig", [
+        'visites' => $visites
         ]);
     }
 }
--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -9,25 +9,45 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * @extends ServiceEntityRepository<Visite>
  */
-class VisiteRepository extends ServiceEntityRepository
-{
-    public function __construct(ManagerRegistry $registry)
-    {
+class VisiteRepository extends ServiceEntityRepository {
+
+    public function __construct(ManagerRegistry $registry) {
         parent::__construct($registry, Visite::class);
     }
-    
+
     /**
      * Retourne toutes les visites triées sur un champ
      * @param type $champ
      * @param type $order
      * @return Visite[]
      */
-    public function findAllOrderBy($champ, $order): array{
+    public function findAllOrderBy($champ, $order): array {
         return $this->createQueryBuilder('v')
-                ->orderBy('v.'.$champ, $order)
-                ->getQuery()
-                ->getResult();
+                        ->orderBy('v.' . $champ, $order)
+                        ->getQuery()
+                        ->getResult();
     }
     
-    
+    /**
+     * Enregistrements dont un champ est égal à une valeur
+     * ou tous les enregistrements si la valeur est vide
+     * @param type $champ
+     * @param type $valeur
+     * @return Vistes[]
+     */
+    public function findByEqualValue($champ, $valeur): array {
+        if ($valeur=="") {
+            return $this->createQueryBuilder('v')
+                            ->orderBy('v.'. $champ, 'ASC')
+                            ->getQuery()
+                            ->getResult();
+        } else {
+            return $this->createQueryBuilder('v')
+                            ->where('v.'. $champ.'=:valeur')
+                            ->setParameter('valeur', $valeur)
+                            ->orderBy('v.datecreation', 'DESC')
+                            ->getQuery()
+                            ->getResult();
+        }
+    }
 }
