19148bfb46251dd069571ce47780c76d35442103
MLDev33 <ypmarielaure@yahoo.fr>
2025-02-09 10:48:38 +0100

    Phase 19 : gestion des mails

--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -2,9 +2,9 @@
 
 namespace App\Form;
 
-use App\Entity\Environnement;
-use App\Entity\Visite;
 use DateTime;
+use App\Entity\Visite;
+use App\Entity\Environnement;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
