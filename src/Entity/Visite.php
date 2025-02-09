f3e8263b11f3b31e8788dc85ead67e85e5c5d7d2
MLDev33 <ypmarielaure@yahoo.fr>
2025-02-09 04:12:52 +0100

    Phase 18 : gestion des images

--- src/Entity/Visite.php
+++ src/Entity/Visite.php
@@ -7,10 +7,15 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 #[ORM\Entity(repositoryClass: VisiteRepository::class)]
-class Visite
-{
+#[Vich\Uploadable]
+class Visite {
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -43,104 +48,98 @@ class Visite
     #[ORM\ManyToMany(targetEntity: Environnement::class)]
     private Collection $environnements;
 
-    public function __construct()
-    {
+    #[Vich\UploadableField(mapping: 'visites', fileNameProperty: 'imageName', size: 'imageSize')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $imageName = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $imageSize = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function __construct() {
         $this->environnements = new ArrayCollection();
     }
 
-
-    public function getId(): ?int
-    {
+    public function getId(): ?int {
         return $this->id;
     }
 
-    public function getVille(): ?string
-    {
+    public function getVille(): ?string {
         return $this->ville;
     }
 
-    public function setVille(string $ville): static
-    {
+    public function setVille(string $ville): static {
         $this->ville = $ville;
 
         return $this;
     }
 
-    public function getPays(): ?string
-    {
+    public function getPays(): ?string {
         return $this->pays;
     }
 
-    public function setPays(string $pays): static
-    {
+    public function setPays(string $pays): static {
         $this->pays = $pays;
 
         return $this;
     }
 
-    public function getDatecreation(): ?\DateTimeInterface
-    {
+    public function getDatecreation(): ?\DateTimeInterface {
         return $this->datecreation;
     }
 
-    public function setDatecreation(?\DateTimeInterface $datecreation): static
-    {
+    public function setDatecreation(?\DateTimeInterface $datecreation): static {
         $this->datecreation = $datecreation;
 
         return $this;
     }
 
-    public function getNote(): ?int
-    {
+    public function getNote(): ?int {
         return $this->note;
     }
 
-    public function setNote(?int $note): static
-    {
+    public function setNote(?int $note): static {
         $this->note = $note;
 
         return $this;
     }
 
-    public function getAvis(): ?string
-    {
+    public function getAvis(): ?string {
         return $this->avis;
     }
 
-    public function setAvis(?string $avis): static
-    {
+    public function setAvis(?string $avis): static {
         $this->avis = $avis;
 
         return $this;
     }
 
-    public function getTempmin(): ?int
-    {
+    public function getTempmin(): ?int {
         return $this->tempmin;
     }
 
-    public function setTempmin(?int $tempmin): static
-    {
+    public function setTempmin(?int $tempmin): static {
         $this->tempmin = $tempmin;
 
         return $this;
     }
 
-    public function getTempmax(): ?int
-    {
+    public function getTempmax(): ?int {
         return $this->tempmax;
     }
 
-    public function setTempmax(?int $tempmax): static
-    {
+    public function setTempmax(?int $tempmax): static {
         $this->tempmax = $tempmax;
 
         return $this;
     }
-    
-    public function getDatecreationString() : string
-    {
-        if($this->datecreation == null){
+
+    public function getDatecreationString(): string {
+        if ($this->datecreation == null) {
             return "";
         } else {
             return $this->datecreation->format('d/m/Y');
@@ -150,13 +149,11 @@ class Visite
     /**
      * @return Collection<int, Environnement>
      */
-    public function getEnvironnements(): Collection
-    {
+    public function getEnvironnements(): Collection {
         return $this->environnements;
     }
 
-    public function addEnvironnement(Environnement $environnement): static
-    {
+    public function addEnvironnement(Environnement $environnement): static {
         if (!$this->environnements->contains($environnement)) {
             $this->environnements->add($environnement);
         }
@@ -164,11 +161,55 @@ class Visite
         return $this;
     }
 
-    public function removeEnvironnement(Environnement $environnement): static
-    {
+    public function removeEnvironnement(Environnement $environnement): static {
         $this->environnements->removeElement($environnement);
 
         return $this;
     }
 
+    public function getImageFile(): ?File {
+        return $this->imageFile;
+    }
+
+    public function getImageName(): ?string {
+        return $this->imageName;
+    }
+
+    public function getImageSize(): ?int {
+        return $this->imageSize;
+    }
+
+    public function setImageFile(?File $imageFile): void {
+        $this->imageFile = $imageFile;
+        if (null !== $imageFile) {
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function setImageName(?string $imageName): void {
+        $this->imageName = $imageName;
+    }
+
+    public function setImageSize(?int $imageSize): void {
+        $this->imageSize = $imageSize;
+    }
+
+    #[Assert\Callback]
+    public function validate(ExecutionContextInterface $context) {
+        $file = $this->getImageFile();
+        if ($file != null && $file != "") {
+            $poids = @filesize($file);
+            if ($poids != false && $poids > 512000) {
+                $context->buildViolation("Cette image est trop lourde (500Ko max)")
+                        ->atPath('imageFile')
+                        ->addViolation();
+            }
+            $infosImage = @getimagesize($file);
+            if ($infosImage == false) {
+                $context->buildViolation("Ce fichier n'est pas une image")
+                        ->atPath('imageFile')
+                        ->addViolation();
+            }
+        }
+    }
 }
--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -8,46 +8,48 @@ use DateTime;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+class VisiteType extends AbstractType {
 
-class VisiteType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
         $builder
-            ->add('ville')
-            ->add('pays')
-            ->add('datecreation', DateType::class, [
-                'widget' => 'single_text',
-                'data' => isset($options['data']) &&
+                ->add('ville')
+                ->add('pays')
+                ->add('datecreation', DateType::class, [
+                    'widget' => 'single_text',
+                    'data' => isset($options['data']) &&
                     $options['data']->getDateCreation() != null ? $options['data']->getDateCreation() : new DateTime('now'),
-                'label' => 'date'
-            ])
-            ->add('note')
-            ->add('avis')
-            ->add('tempmin', null, [
-                'label' => 't° min'
-            ])
-            ->add('tempmax', null, [
-                'label' => 't° max'
-            ])
-            ->add('environnements', EntityType::class, [
-                'class' => Environnement::class,
-                'choice_label'=> 'nom',
-                'multiple' => true,
-                'required' => false
-        ])
-            ->add('submit', SubmitType::class, [
-                'label' => 'Enregistrer'
-            ])
+                    'label' => 'date'
+                ])
+                ->add('note')
+                ->add('avis')
+                ->add('tempmin', null, [
+                    'label' => 't° min'
+                ])
+                ->add('tempmax', null, [
+                    'label' => 't° max'
+                ])
+                ->add('environnements', EntityType::class, [
+                    'class' => Environnement::class,
+                    'choice_label' => 'nom',
+                    'multiple' => true,
+                    'required' => false
+                ])
+                ->add('imageFile', FileType::class, [
+                    'required' => false,
+                    'label' => 'selection image'
+                ])
+                ->add('submit', SubmitType::class, [
+                    'label' => 'Enregistrer'
+                ])
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
+    public function configureOptions(OptionsResolver $resolver): void {
         $resolver->setDefaults([
             'data_class' => Visite::class,
         ]);
