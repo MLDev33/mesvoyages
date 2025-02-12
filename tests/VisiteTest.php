2bb747e0c6fa188b45b19382e67a2dc82f7f4943
MLDev33 <ypmarielaure@yahoo.fr>
2025-02-12 21:48:52 +0100

    Phase 23 : tests (compléments)

--- tests/VisiteTest.php
+++ tests/VisiteTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests;
 
 use App\Entity\Visite;
+use App\Entity\Environnement;
 use DateTime;
 use PHPUnit\Framework\TestCase;
 
@@ -12,10 +13,21 @@ use PHPUnit\Framework\TestCase;
  * @author m-lordiportable
  */
 class VisiteTest extends TestCase {
-    
-    public function testGetDatecreationString(){
+
+    public function testGetDatecreationString() {
         $visite = new Visite();
         $visite->setDatecreation(new DateTime("2024-04-24"));
         $this->assertEquals("24/04/2024", $visite->getDatecreationString());
+    }
+
+    public function testAddEnvironnement() {
+        $environnement = new Environnement();
+        $environnement->setNom("plage");
+        $visite = new Visite();
+        $visite->addEnvironnement($environnement);
+        $nbEnvironnementAvant = $visite->getEnvironnements()->count();
+        $visite->addEnvironnement($environnement);
+        $nbEnvironnementApres = $visite->getEnvironnements()->count();
+        $this->assertEquals($nbEnvironnementAvant, $nbEnvironnementApres, "ajout même environnement devrait échouer");
     }
 }
